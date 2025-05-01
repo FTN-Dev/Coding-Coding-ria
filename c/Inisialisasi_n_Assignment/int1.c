f8afe8f5e50bc4cc31bf7507bd7f4de3df13ef0b
Always Trying To Be Better <103570217+FTN-Dev@users.noreply.github.com>
2025-05-01 22:43:02 +0700

    Add files via upload
    c

---
+++ c/Inisialisasi_n_Assignment/int1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+// int main(){
+//     int i = 5;
+//     printf("ini adalah angka dari i: %d \n", i);
+//     return 0;
+// }
+
+// Dibawah ini adalah tugas tambahan
+int main(){
+    int bebek = 10;
+    float luasrumah = 2.5;
+    float lingkaran1 = 2*3.14*5.7;
+    printf("aku punya %d bebek \n", bebek);
+    printf("aku punya rumah seluas %.1f meter \n", luasrumah);
+    printf("keliling lingkaranku %.4f", lingkaran1);
+    return 0;
+}
