00ec2808790bd2fa32127e43bd022d47cab0f53e
Always Trying To Be Better <103570217+FTN-Dev@users.noreply.github.com>
2025-05-02 09:13:07 +0700

    c

---
+++ c/Inisialisasi_n_Assignment/assigment.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+
+    int i; //<--- state pertama
+
+    i = 5; //assignment pertama
+    printf("ini adalah assigment pertama: %d \n", i);
+
+    i = i+1; //assignment kedua
+    printf("ini adalah assigment kedua: %d \n", i);
+}
