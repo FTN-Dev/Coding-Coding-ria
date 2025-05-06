8834cc492eb4854976494e8819f188a605d13a97
Always Trying To Be Better <103570217+FTN-Dev@users.noreply.github.com>
2025-05-06 09:35:14 +0700

    c

---
+++ c/Baca-Tulis/scanf.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    int a;
+    float f;
+
+    // printf("Masukkan nilai integer: ");
+    // scanf("%d", &a);
+    // printf("Ini adalah nilai yang engkau input: %d \n \n", a);
+
+    // printf("Masukkan nilai float/ bilangan ril: ");
+    // scanf("%f", &f);
+    // printf("Ini adalah nilai inputmu: %f \n", f);
+
+    // tambahan, aku ingin coba input strings
+    char nama[50];
+    printf("Masukkan namamu: ");
+    scanf("%s", &nama);
+    printf("Hai %s!", nama);
+}
