0bc45b200801cf8f9e418022a98600be5e621245
Always Trying To Be Better <103570217+FTN-Dev@users.noreply.github.com>
2025-05-15 17:03:50 +0700

    C base on Dicoding Academy

--- c/loop/while-do.c
+++ c/loop/while-do.c
@@ -30,3 +30,19 @@
 // }
 
 
+// Creative Activity 2
+// Diubah menjadi seperti yang di loop, menggunakan scanf
+
+int main(){
+    
+    int jawaban;
+    int index = 0;
+
+    printf("Sebutkan ingin berapa kali loop untuk mengeluarkan kalimat \"Aku Suka C\": ");
+    scanf("%d", &jawaban);
+    while( index < jawaban ) {
+        printf("Aku Suka C Part 2\n");
+        index++;
+    }
+    return 0;
+}
