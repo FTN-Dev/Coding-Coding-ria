8958838103903eed47f73f24a5c2217e59023667
Always Trying To Be Better <103570217+FTN-Dev@users.noreply.github.com>
2025-05-15 18:56:27 +0700

    C Base on Dicoding Academy

--- c/loop/while-do/while-do.c
+++ c/loop/while-do/while-do.c
@@ -13,36 +13,3 @@ int main() {
 
 
 // Jadi perbedaannya dengan for adalah, bahwa index disini harus distate terlebih dahulu berapa, contoh disini distate jadi i = 0; pada variable, namun jika menggunakan for, cukup int i; Jadi cukup dijadikan variable yang ada.
-
-
-// Creative Activity 1
-// Cara agar loop tidak berjalan, dengan cara variable tidak distate berapa.
-
-// int main() {
-
-//     int i;
-
-//     while(i<3) {
-//         printf("Aku suka C Part 2 \n");
-//         i++;
-//     }
-
-// }
-
-
-// Creative Activity 2
-// Diubah menjadi seperti yang di loop, menggunakan scanf
-
-int main(){
-    
-    int jawaban;
-    int index = 0;
-
-    printf("Sebutkan ingin berapa kali loop untuk mengeluarkan kalimat \"Aku Suka C\": ");
-    scanf("%d", &jawaban);
-    while( index < jawaban ) {
-        printf("Aku Suka C Part 2\n");
-        index++;
-    }
-    return 0;
-}
