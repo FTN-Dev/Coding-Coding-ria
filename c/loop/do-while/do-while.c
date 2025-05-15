12575d889cf04030564e69f0f7b721dc2a1bdaee
Always Trying To Be Better <103570217+FTN-Dev@users.noreply.github.com>
2025-05-15 23:32:06 +0700

    do-while

---
+++ c/loop/do-while/do-while.c
@@ -0,0 +1,42 @@
+// #include <stdio.h>
+
+// int main() {
+
+//     // int i;
+//     int i = 0;
+
+//     do {
+
+//         printf("Aku suka C Part 3 \n");
+//         i++;
+
+//     } while (i < 3);
+//     return 0;
+// }
+
+// Ya, ternyata masih sama, i harus dideklarasi dulu sebagai berapa. Tidak hanya int i;
+// Jangan lupa i++, gua lupa terus jir😹
+
+
+
+// Creative Activity
+
+#include <stdio.h>
+
+int main() {
+
+    int index = 0;
+    int jawaban;
+
+    printf("\"Aku suka C Part 3\"\n");
+    printf("Input angka untuk berapa kali kamu ingin loop kalimat diatas: ");
+    scanf("%d", &jawaban);
+
+    do {
+
+        printf("Aku Suka C Part 3 \n");
+        index++;
+
+    } while( index < jawaban );
+
+}
