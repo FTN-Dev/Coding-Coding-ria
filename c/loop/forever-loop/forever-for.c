461d6d3696ef89de186a7a90a27f02591d62c1a2
Always Trying To Be Better <103570217+FTN-Dev@users.noreply.github.com>
2025-05-16 00:18:19 +0700

    for

---
+++ c/loop/forever-loop/forever-for.c
@@ -0,0 +1,16 @@
+// Forever for loop
+#include <stdio.h>
+
+// Forever Loop For
+int main() {
+
+    printf("Loop ini menggunakan Forever Loop For\n");
+    printf("Loop ini hanya bisa dihentikan menggunakan ^C (Control + C)\n");
+    for(;;) {
+
+    printf("Tunggu ^C ...\n");
+    
+    }
+    return 0;
+
+}
