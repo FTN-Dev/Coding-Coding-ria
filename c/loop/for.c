bcda55e82e071c21dee45eaf141d9f883138bca5
Always Trying To Be Better <103570217+FTN-Dev@users.noreply.github.com>
2025-05-15 16:42:46 +0700

    C base on Dicoding Academy

--- c/loop/for.c
+++ c/loop/for.c
@@ -1,13 +1,27 @@
 #include <stdio.h>
 
-int main() {
+// int main() {
 
-    int i;
+//     int i;
 
-    for(i=0; i < 3; i++){
-        printf("Aku suka C \n");
+//     for(i=0; i < 3; i++) {
+//         printf("Aku suka C \n");
+//     }
+
+//     return 0;
+// }
+
+// Creative Acitivity
+
+int main(){
+    
+    int jawaban;
+    int index;
+
+    printf("Sebutkan ingin berapa kali loop untuk mengeluarkan kalimat \"Aku Suka C\": ");
+    scanf("%d", &jawaban);
+    for( index = 0; index < jawaban; index++ ) {
+        printf("Aku Suka C \n");
     }
-
     return 0;
 }
-
