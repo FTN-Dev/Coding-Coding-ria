9e9d935fab85debab3e7297d943df8d78ca8fad6
Always Trying To Be Better <103570217+FTN-Dev@users.noreply.github.com>
2025-05-25 18:00:20 +0700

    c sorting

---
+++ c/array-tabel/max-sort/max-sort.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+int main(){
+    
+    int Tab[10]= { 1, 50 , 6 , 200, 3, 100, 30, 8, 99, 100 };
+
+    int max;
+    int k, temp;
+
+    for (int i = 0; i<9; i++ ){
+        max = i;
+        for (k= i+1 ;k< 10; k++)  {
+            if (Tab[k]> Tab[max]) {
+                max=k;
+            }
+        } 
+ 
+        temp= Tab[i];
+        Tab[i]= Tab[max];
+        Tab[max] = temp;
+        
+    }
+
+    for (int i=0;i<10;i++) {
+        printf (" %d ; ", Tab[i]);
+    }
+
+    return 0;
+}
+
+// Creative Acitivity, sorting from minimum
+
+#include<stdio.h>
+int main(){
+    
+    int Tab[10]= { 1, 50 , 6 , 200, 3, 100, 30, 8, 99, 100 };
+
+    int max;
+    int k, temp;
+
+    for (int i = 0; i<9; i++ ){
+        max = i;
+        for (k= i+1 ;k< 10; k++)  {
+            if (Tab[k] < Tab[max]) { //tinggal diubah disini jir
+                max=k;
+            }
+        } 
+ 
+        temp= Tab[i];
+        Tab[i]= Tab[max];
+        Tab[max] = temp;
+        
+    }
+
+    for (int i=0;i<10;i++) {
+        printf (" %d ; ", Tab[i]);
+    }
+
+    return 0;
+}
+
+// honestly, bagian yang ini kurang paham
